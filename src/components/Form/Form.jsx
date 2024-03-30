dbc6d134bcea4153e46849cf8f9b6d441fd5b285
co11aps <oleksandr.fedorov@gmail.com>
2024-03-31 02:31:08 +0800

    1111

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,27 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({ onSubmit }) => {
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    const text = evt.target.search.value;
+    onSubmit(text);
+    evt.target.reset();
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+      />
+    </form>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,35 @@
-import { Text } from 'components';
+import { Form, Text, TodoList } from 'components';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todos, setTodos] = useState([
+    {
+      id: '1',
+      text: 'text',
+    },
+    {
+      id: '2',
+      text: 'text-2',
+    },
+  ]);
+
+  const addToDo = text => {
+    const newToDo = {
+      id: nanoid(),
+      text,
+    };
+    setTodos([...todos, newToDo]);
+  };
+
+  return (
+    <>
+      <Form onSubmit={addToDo} />
+      {todos.length > 0 ? (
+        <TodoList toDos={todos} />
+      ) : (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
+    </>
+  );
 };
